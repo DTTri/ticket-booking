09451795fafa3b40766ec78c24af626225b33f29
Tri Duong <duongthuantri159@gmail.com>
2025-04-19 10:43:10 +0700

    Commit for merging

---
+++ client/src/app/orders/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function OrdersPage() {
+  return (
+    <div className="w-full pt-4">
+      <div className="mx-auto w-full max-w-[620px] flex flex-col items-center gap-8"></div>
+    </div>
+  );
+}
